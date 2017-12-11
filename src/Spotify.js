110f46d56a0b7b2080eff002b2474c6699586315
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-10 22:07:25 -0200

    Add the AlbumTrackS to view.

--- src/Spotify.js
+++ src/Spotify.js
@@ -2,7 +2,7 @@ import SpotifyWrapper from 'spotify-wrapper-tdd-course';
 
 const spotify = new SpotifyWrapper({
   // eslint-disable-next-line quotes
-  token: `BQAVcAV8hOXj4c8Q_qAG0deahhMqsHLRH9DASIBWupBbtq6T3HjMG0IvoKkDNif0Fi2D-ztr5wQMl2I9rb5IMU-eUhXvmzcCI4arxmxca890r6NmMZHcffx1zOhU3u1iiLnASu67vMMeng`,
+  token: `BQB3p1oJOAspRfTW33pnji1pk4oKOepzBExJRHh80EoxgsXfFcLAGgchPPMO_-J5T7oBuO0RRnLMgQ7A7ek_jh9JtCyUPyoOwEssMc936E7CIeS86v6jvpFqrlU_vgfCaki_MhMydothZg`,
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -6,16 +6,14 @@ import renderAlbumTracs from './AlbumTracks';
 const albums = spotify.search.albums('Incubus');
 // eslint-disable-next-line no-undef
 const albumList = document.getElementById('album-list');
-
-const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
+const album = spotify.album.getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
 // eslint-disable-next-line no-undef
 const albumInfo = document.getElementById('album-info');
-
-
-// const tracks = spotify.album.getAlbumTracks('');
-
+// eslint-disable-next-line no-undef
+const albumTracks = document.getElementById('album-tracks');
 
 albums.then(data => renderAlbums(data.albums.items, albumList));
 
-album.then(data => renderAlbumInfo(data, albumInfo))
-
+album
+  .then(data => renderAlbumInfo(data, albumInfo))
+  .then(data => renderAlbumTracs(data.tracks.items, albumTracks));
