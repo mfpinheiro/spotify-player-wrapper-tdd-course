427c90fdee4ee1f16c0bcca34a946750bc529874
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-10 22:37:56 -0200

    Add formmated time to track list.

--- tests/AlbumTracks.spec.js
+++ tests/AlbumTracks.spec.js
@@ -1,13 +1,14 @@
 import 'jsdom-global/register';
 import { expect } from 'chai';
 import renderAlbumTracks from '../src/AlbumTracks';
+import convertToHumanTime from '../src/convertToHumanTime';
 
 describe('Album Tracks', () => {
   const oneTrackMarkup = `
       <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/c1949d93cb9ea4970da80f827ea6e9f4c33ebc4a?cid=8897482848704f2a8f8d7c79726a70d4">
         <p class="music-number">1</p>
         <p class="music-title">Version</p>
-        <p class="music-duration">214946</p>
+        <p class="music-duration">${convertToHumanTime(214946)}</p>
       </div>`;
 
   const oneTrackData = [
@@ -41,12 +42,12 @@ describe('Album Tracks', () => {
       <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/c1949d93cb9ea4970da80f827ea6e9f4c33ebc4a?cid=8897482848704f2a8f8d7c79726a70d4">
         <p class="music-number">1</p>
         <p class="music-title">Version</p>
-        <p class="music-duration">214946</p>
+        <p class="music-duration">${convertToHumanTime(214946)}</p>
       </div>
       <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/c1949d93cb9ea4970da80f827ea6e9f4c33ebc4a?cid=8897482848704f2a8f8d7c79726a70d4">
         <p class="music-number">1</p>
         <p class="music-title">Version</p>
-        <p class="music-duration">214946</p>
+        <p class="music-duration">${convertToHumanTime(214946)}</p>
       </div>`;
 
   it('should create and append the markup given the correct data', () => {
