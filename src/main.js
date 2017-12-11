b1a42c4c62d5ce11819501a8ebd9aa3e669c0983
mfpinheiro <mateusfpinheiro@gmail.com>
2017-12-10 22:48:46 -0200

    Add events to trigger the search the albums of a band.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 import spotify from './Spotify';
-import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
 import renderAlbumTracs from './AlbumTracks';
+import searchEnterTrigger from './searchTrigger';
 
-const albums = spotify.search.albums('Incubus');
-// eslint-disable-next-line no-undef
-const albumList = document.getElementById('album-list');
+searchEnterTrigger();
+
 const album = spotify.album.getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
 // eslint-disable-next-line no-undef
 const albumInfo = document.getElementById('album-info');
 // eslint-disable-next-line no-undef
 const albumTracks = document.getElementById('album-tracks');
-
-albums.then(data => renderAlbums(data.albums.items, albumList));
 
 album
   .then(data => renderAlbumInfo(data, albumInfo))
---
+++ src/searchTrigger.js
@@ -0,0 +1,18 @@
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+// eslint-disable-next-line no-undef
+const albumList = document.getElementById('album-list');
+// eslint-disable-next-line no-undef
+const searchInput = document.getElementById('search-input');
+// eslint-disable-next-line no-undef
+const searchFrom = document.getElementById('search-form');
+
+export default function searchEnterTrigger() {
+  searchFrom.addEventListener('submit', e => {
+    e.preventDefault();
+    spotify.search
+      .albums(searchInput.value)
+      .then(data => renderAlbums(data.albums.items, albumList));
+  });
+}
